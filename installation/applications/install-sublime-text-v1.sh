18156fdbab39b4d27723f3f5fcdccb25438c3f19
rop2bash <rop2bash@gmail.com>
2018-08-18 21:53:59 +0200

    fixed sublime-text source

--- installation/applications/install-sublime-text-v1.sh
+++ installation/applications/install-sublime-text-v1.sh
@@ -11,13 +11,7 @@
 #
 ##################################################################################################################
 
-rm /tmp/sublime-text_build-3126_amd64.deb
-
-wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb -O /tmp/sublime-text_build-3126_amd64.deb
-sudo dpkg -i /tmp/sublime-text_build-3126_amd64.deb
-
-rm /tmp/sublime-text_build-3126_amd64.deb
-
+sudo apt install sublime-text
 
 ##################################################################################################################
 
