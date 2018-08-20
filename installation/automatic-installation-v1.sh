de130304072aee6d8a38c5f27ac4093aafaaac11
rop2bash <rop2bash@gmail.com>
2018-08-20 02:40:24 +0200

    --

--- installation/automatic-installation-v1.sh
+++ installation/automatic-installation-v1.sh
@@ -20,7 +20,7 @@ sh 430-install-applications-v1.sh
 
 sh 600-copy-personal-settings.sh
 
-sh 900-copy-i3-files-to-config-i3-folder-v2.sh
+sh 900-copy-config-files-v1.sh
 
 echo "###################################################"
 echo "##############     Auto script terminated     #################"
