66ecf8bd93371c5b5b4130e94e7004f1f019ef46
rop2bash <rop2bash@gmail.com>
2019-01-24 18:23:41 +0100

    ++--

--- .config/polybar/scripts/clementine_status.sh
+++ .config/polybar/scripts/clementine_status.sh
@@ -7,7 +7,7 @@ if [[ $LAUNCHED != "" ]]; then
 	#clementine
 	ARTIST=$(qdbus org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get org.mpris.MediaPlayer2.Player Metadata | grep artist | cut -d ' ' -f2-)
 	TITLE=$(qdbus org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get org.mpris.MediaPlayer2.Player Metadata | grep title  | cut -d ' ' -f2-)
-
+	STATUS=$(qdbus org.mpris.MediaPlayer2.clementine /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get org.mpris.MediaPlayer2.Player PlaybackStatus)
 	if [[ $ARTIST != "" ]]; then
 	  OUTPUT=$ARTIST
 	fi
@@ -16,7 +16,13 @@ if [[ $LAUNCHED != "" ]]; then
 	  OUTPUT="$OUTPUT: $TITLE"
 	fi
 
+	if [[ $STATUS == "Playing" ]]; then
+		OUTPUT=" $OUTPUT"
+	else
+		OUTPUT=" $OUTPUT"
+	fi
+
 else
-	OUTPUT=""	
+	OUTPUT="Clementine"	
 fi
 echo "$OUTPUT"
